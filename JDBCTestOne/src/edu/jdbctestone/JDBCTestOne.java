91a6ffd4aecab09d0a2bdd37b01a6e3d75de0324
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-22 18:46:59 +0900

    second

--- JDBCTestOne/src/edu/jdbctestone/JDBCTestOne.java
+++ JDBCTestOne/src/edu/jdbctestone/JDBCTestOne.java
@@ -16,7 +16,7 @@ public class JDBCTestOne {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/" 
-					+ "sampledb?useSSL=" +
+					+ "malldb?useSSL=" +
 							"false&serverTimezone=Asia/Seoul",
 							"root", "895029qaz");
 			
@@ -24,46 +24,82 @@ public class JDBCTestOne {
 				System.out.println("DB 연결 완료");
 				stmt = conn.createStatement();
 				ResultSet srs = stmt.executeQuery(
-						"select * from student");
-				printData(srs, "name", "id", "dept");
+						"select * from userTbl");
+				printData(srs, "userID", "name", "birthYear", "addr", "mobile1", "mobile2", "height", "mDate");
+				ResultSet srs1 = stmt.executeQuery(
+						"select * from buyTbl");
+				
+				printData1(srs1, "num", "userID", "prodName", "groupName", "price", "amount");
 			}
 		} catch (ClassNotFoundException e) {
 			System.out.println("JDBC 드라이버 로드 오류");
 		} catch (SQLException e) {
-			System.out.println("DB dus결 오류");
+			System.out.println("DB 연결 오류");
 		}
 	}
-//		finally {
-//			try {
-//				stmt.close();
-//				conn.close();
-//			}
-//			
-//			catch(Exception e) {
-//				System.out.println(e.getMessage());
-//			}
-//			
-//			
-//		}
-//		}
+
 
 	   private static void printData(
 		         ResultSet srs, 
-		         String col1, String col2,
-		         String col3) throws SQLException {
+		         String col1, String col2, String col3,
+		         String col4, String col5, String col6, String col7, String col8) throws SQLException {
 		      while (srs.next()) {
 		         if (!col1.equals(""))
-		            System.out.print(srs.getString("name")); 
+		            System.out.println(srs.getString("userID")); 
 		         if (!col2.equals(""))
 		            System.out.print("\t|\t" + 
-		                  srs.getString("id"));
+		                  srs.getString("name"));
 		         if (!col3.equals(""))
-		            System.out.println("\t|\t" + 
-		                  srs.getString("dept"));
+		            System.out.print("\t|\t" + 
+		                  srs.getString("birthYear"));
+		         if (!col4.equals(""))
+			            System.out.print("\t|\t" + 
+			                  srs.getString("addr"));
+		         if (!col5.equals(""))
+			            System.out.print("\t|\t" + 
+			                  srs.getString("mobile1"));
+		         if (!col6.equals(""))
+			            System.out.print("\t|\t" + 
+			                  srs.getString("mobile2"));
+		         if (!col7.equals(""))
+			            System.out.print("\t|\t" + 
+			                  srs.getString("height"));
+		         if (!col8.equals(""))
+			            System.out.print("\t|\t" + 
+			                  srs.getString("mDate"));
 		         else 
 		            System.out.println();
-		      }//end of while(rs.next() )
+		      }
 	   }
-}
+	
+		private static void printData1(
+		        ResultSet srs, 
+		        String col1, String col2, String col3,
+		        String col4, String col5, String col6) throws SQLException {
+		     while (srs.next()) {
+		        if (!col1.equals(""))
+		           System.out.println(srs.getString("num")); 
+		        if (!col2.equals(""))
+		           System.out.print("\t|\t" + 
+		                 srs.getString("userID"));
+		        if (!col3.equals(""))
+		            System.out.print("\t|\t" + 
+		                  srs.getString("prodName"));
+		        if (!col4.equals(""))
+		           System.out.print("\t|\t" + 
+		                 srs.getString("groupName"));
+		        if (!col5.equals(""))
+			            System.out.print("\t|\t" + 
+			                  srs.getString("price"));
+		        if (!col6.equals(""))
+		        	System.out.print("\t|\t" + srs.getString("amount"));
+		        
+		       
+		        else 
+		           System.out.println();
+		     }
+		}
+	}
+
 
 		
